91a4c9bdc893ad1bf17f68f210bf101a120c1bd1
DanilSaidov <danil.saidov.2012@mail.ru>
2021-01-19 14:54:11 +0300

    'heroku'

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./public/db.json");
-cost middlewares = jsonServer.defaults({
-    static:'./build'
-}) 
+const middlewares = jsonServer.defaults({
+  static: "./build",
+});
+const PORT = process.env.PORT || 3001;
+server.use(middlewares);
+server.use(router);
+server.listen(PORT, () => {
+  console.log("Server is starting");
+});
